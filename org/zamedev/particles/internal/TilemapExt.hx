f2391b756e49204b4dff30472439e52b8ee7ec6e
Michael Engel <michael.engel@innogames.com>
2026-02-27 15:33:31 +0100

    add fix for openfl9

--- org/zamedev/particles/internal/TilemapExt.hx
+++ org/zamedev/particles/internal/TilemapExt.hx
@@ -1,27 +1,21 @@
 package org.zamedev.particles.internal;
 
 import openfl.display.Tilemap;
-
 #if !flash
-
 import openfl.display.DisplayObject;
 
 class TilemapExt extends Tilemap {
-    private override function __hitTest(
-        x : Float,
-        y : Float,
-        shapeFlag : Bool,
-        stack : Array<DisplayObject>,
-        interactiveOnly : Bool,
-        hitObject : DisplayObject,
-        hitTestWhenMouseDisabled:Bool = false
-    ) : Bool {
-        return false;
-    }
+	#if (openfl >= "9.0.0")
+	private override function __hitTest(x:Float, y:Float, shapeFlag:Bool, stack:Array<DisplayObject>, interactiveOnly:Bool, hitObject:DisplayObject):Bool {
+		return false;
+	}
+	#else
+	private override function __hitTest(x:Float, y:Float, shapeFlag:Bool, stack:Array<DisplayObject>, interactiveOnly:Bool, hitObject:DisplayObject,
+			hitTestWhenMouseDisabled:Bool):Bool {
+		return false;
+	}
+	#end
 }
-
 #else
-
 typedef TilemapExt = Tilemap;
-
 #end
